28205ebab1d5c481d425669bed706a6fba18a8fa
Swapneel Bobba <swapneel.bobba123@gmail.com>
2022-10-08 20:15:09 +0100

    initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("daisyui"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e11d48",
+
+          secondary: "#fb7185",
+
+          accent: "#37CDBE",
+
+          neutral: "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+};
