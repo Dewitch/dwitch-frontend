0b7bfb3c19b0a56b23b55471e25ed02830b6e1be
Swapneel Bobba <dilano.bo@protonmail.com>
2022-10-09 18:45:00 +0100

    post to create stream object

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,17 +28,51 @@ export default function Home() {
 
   useEffect(() => {
     const subscribeToStream = async () => {
-      const axiosInstance = axios.create({
+      const data = {
+        name: "test_stream",
+        profiles: [
+          {
+            name: "720p",
+            bitrate: 2000000,
+            fps: 30,
+            width: 1280,
+            height: 720,
+          },
+          {
+            name: "480p",
+            bitrate: 1000000,
+            fps: 30,
+            width: 854,
+            height: 480,
+          },
+          {
+            name: "360p",
+            bitrate: 500000,
+            fps: 30,
+            width: 640,
+            height: 360,
+          },
+        ],
+      };
+
+      const config = {
         headers: {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_LIVEPEER_KEY}`,
         },
-      });
-      axiosInstance
-        .post("https://livepeer.studio/api/stream")
-        .then((response) => setStream(response.data));
+      };
+
+      await axios
+        .post("https://livepeer.studio/api/stream", data, config)
+        .then((response) => {
+          console.log({ response });
+          setStream(response.data);
+        })
+        .catch((error) => console.log("error: ", error));
     };
     subscribeToStream();
   }, []);
+
+  console.log("stream state: ", stream);
 
   const images = [
     {
