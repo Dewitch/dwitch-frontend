c4e0a7bdc6cd4d72e5456d3210d26a2a12bf9723
Kinetic <kinetic@Kinetics-MacBook-Pro.local>
2022-10-09 02:31:34 -0500

    added in the provider but with ethers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,36 +8,62 @@ import {
   Theme,
   lightTheme,
 } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-import { publicProvider } from "wagmi/providers/public";
+import { chain, createClient, WagmiConfig } from "wagmi";
+import ethers from "ethers";
 
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.goerli],
-  // [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
-  [
-    jsonRpcProvider({
-      rpc: (chain) => ({
-        // http: `https://${chain.id}.example.com`,
-        http: `https://goerli.ethereum.coinbasecloud.net`,
-      }),
-    }),
-  ]
-);
+// import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+// import { alchemyProvider } from "wagmi/providers/alchemy";
+// import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+// import { publicProvider } from "wagmi/providers/public";
 
-const { connectors } = getDefaultWallets({
-  appName: "Dwitch",
-  chains,
-});
+// const { chains, provider } = configureChains(
+//   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.goerli],
+//   // [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+//   [
+//     jsonRpcProvider({
+//       rpc: (chain) => ({
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
+//         // http: `https://${chain.id}.example.com`,
+//         http: `https://goerli.ethereum.coinbasecloud.net`,
+//       }),
+//     }),
+//   ]
+// );
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const chains = [
+    chain.mainnet,
+    chain.polygon,
+    chain.optimism,
+    chain.arbitrum,
+    chain.goerli,
+  ];
+
+  // Import environment variables
+  const baseUrl = process.env.NODE_ENDPOINT || "";
+  const username = process.env.NODE_USERNAME || "";
+  const password = process.env.NODE_PASSWORD || "";
+  console.log("ethers");
+  console.log(ethers);
+
+  // Create node provider using project credentials
+  const provider = new ethers.providers.JsonRpcProvider({
+    url: baseUrl,
+    user: username,
+    password: password,
+  });
+
+  const { connectors } = getDefaultWallets({
+    appName: "Dwitch",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider
